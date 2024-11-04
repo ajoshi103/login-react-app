b8493bd4135646a8809826c288029981191b2775
saskue7 <narutoank2016@gmail.com>
2024-11-04 14:19:08 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
 import { Login } from "./components/Login";
 import { Sign } from "./components/Sign";
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
- 
+  const [currentForm, setCurrentForm] = useState("login");
+
   const toggleForm = (formName) => {
     setCurrentForm(formName);
-  }
+  };
 
   return (
     <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Sign onFormSwitch={toggleForm} />
-      }
+   
+      {currentForm === "login" ? (
+        <Login onFormSwitch={toggleForm} />
+      ) : (
+        <Sign onFormSwitch={toggleForm} />
+      )}
     </div>
   );
 }
--- src/components/Sign.js
+++ src/components/Sign.js
@@ -1,84 +1,88 @@
 import React, { useState } from "react";
 
+
 export const Sign = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const [name, setName] = useState('');
-    const [errors, setErrors] = useState({});
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [name, setName] = useState("");
+  const [errors, setErrors] = useState({});
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const validationErrors = {};
-        
-        // Validate name
-        if (!name) {
-            validationErrors.name = "Full name is required";
-        }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = {};
 
-        // Validate email
-        if (!email) {
-            validationErrors.email = "Email is required";
-        } else if (!/\S+@\S+\.\S+/.test(email)) {
-            validationErrors.email = "Email address is invalid";
-        }
-
-        // Validate password
-        if (!pass) {
-            validationErrors.password = "Password is required";
-        } else if (pass.length < 6) {
-            validationErrors.password = "Password must be at least 6 characters long";
-        }
-
-        if (Object.keys(validationErrors).length > 0) {
-            setErrors(validationErrors);
-        } else {
-            alert("user is registered");
-            console.log(email);
-          
-            setErrors({}); // Clear errors
-        }
+    // Validate name
+    if (!name) {
+      validationErrors.name = "Full name is required";
     }
 
-    return (
-        <div className="auth-form-container">
-            <h2>Register</h2>
-            <form className="register-form" onSubmit={handleSubmit}>
-                <label htmlFor="name">Full name</label>
-                <input 
-                    value={name} 
-                    name="name" 
-                    onChange={(e) => setName(e.target.value)} 
-                    id="name" 
-                    placeholder="Full Name" 
-                />
-                {errors.name && <p className="error">{errors.name}</p>}
-                
-                <label htmlFor="email">Email</label>
-                <input 
-                    value={email} 
-                    onChange={(e) => setEmail(e.target.value)} 
-                    type="email" 
-                    placeholder="youremail@gmail.com" 
-                    id="email" 
-                    name="email" 
-                />
-                {errors.email && <p className="error">{errors.email}</p>}
-                
-                <label htmlFor="password">Password</label>
-                <input 
-                    value={pass} 
-                    onChange={(e) => setPass(e.target.value)} 
-                    type="password" 
-                    placeholder="********" 
-                    id="password" 
-                    name="password" 
-                />
-                {errors.password && <p className="error">{errors.password}</p>}
-                
-                <button type="submit " >Register</button>
-            </form>
-            {/* Button to switch to Login */}
-            <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
-        </div>
-    )
-}
+    // Validate email
+    if (!email) {
+      validationErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      validationErrors.email = "Email address is invalid";
+    }
+
+    // Validate password
+    if (!pass) {
+      validationErrors.password = "Password is required";
+    } else if (pass.length < 6) {
+      validationErrors.password = "Password must be at least 6 characters long";
+    }
+
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+     
+      console.log(email);
+     
+
+      setErrors({}); // Clear errors
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <h2>SignUp</h2>
+      <form className="register-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Full name</label>
+        <input
+          value={name}
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          id="name"
+          placeholder="Full Name"
+        />
+        {errors.name && <p className="error">{errors.name}</p>}
+
+        <label htmlFor="email">Email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="youremail@gmail.com"
+          id="email"
+          name="email"
+        />
+        {errors.email && <p className="error">{errors.email}</p>}
+
+        <label htmlFor="password">Password</label>
+        <input
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+        />
+        {errors.password && <p className="error">{errors.password}</p>}
+
+        <button type="submit ">SignUp</button>
+      </form>
+      {/* Button to switch to Login */}
+      <button className="link-btn" onClick={() => props.onFormSwitch("login")}>
+        Already have an account? Login here.
+      </button>
+    </div>
+  );
+};
