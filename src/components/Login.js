b9f0a1fb3b362dd355917ac22ae45356aa9177fe
saskue7 <narutoank2016@gmail.com>
2024-11-04 13:27:59 +0530

    changed

---
+++ src/components/Login.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [errors, setErrors] = useState({});
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const validationErrors = {};
+
+        // Validate email
+        if (!email) {
+            validationErrors.email = "Email is required";
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            validationErrors.email = "Email address is invalid";
+        }
+
+
+
+        // Validate password
+        if (!pass) {
+            validationErrors.password = "Password is required";
+        } else if (pass.length < 6) {
+            validationErrors.password = "Password must be at least 6 characters long";
+        }
+
+        if (Object.keys(validationErrors).length > 0) {
+            setErrors(validationErrors);
+        } else {
+            console.log("Logging in with:", { email, pass }); // Replace with your login logic
+            setErrors({}); // Clear errors
+        }
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Logn</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} 
+                    type="email" 
+                    placeholder="youremail@gmail.com" 
+                    id="email" 
+                    name="email" 
+                />
+                {errors.email && <p className="error">{errors.email}</p>}
+                
+                <label htmlFor="password">Password</label>
+                <input 
+                    value={pass} 
+                    onChange={(e) => setPass(e.target.value)} 
+                    type="password" 
+                    placeholder="********" 
+                    id="password" 
+                    name="password" 
+                />
+                {errors.password && <p className="error">{errors.password}</p>}
+                
+                <button type="submit">Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+    )
+}
